810a64fd799b2282d5dcb4c478ea43acf4bd14f9
takesh111 <150744295+takesh111@users.noreply.github.com>
2026-09-21 18:38:47 +0200

    Fix lesson 1.1 after review

--- stage_01/lesson_01.py
+++ stage_01/lesson_01.py
@@ -1,32 +1,33 @@
-#Задача номер №1
-x=[10,20]
-y=x
+#Задача номер 1
+x = [10,20]
+y = x
 y.append(30)
 print(x)
-y=[99]
+#[10,20,30]
+y = [99]
 print(x,y)
 #[10,20,30],[99]
 
-#Задача номер №2 а - не изменится, потому что в этой строчке b принимает значение a:b = b + "!" и 
-# b будет равно "hello!", а а будет равно просто "hello"
+#Задача номер 2 а - так как строка неизменяемая, b создает новый объект, а "a" остается без 
+# изменений и висит на старом объекте
 a = "hello"
 b = a
 b = b + "!"
 print(a,b)
 
-#Задача номер №3
+#Задача номер 3
 p = [1,2]
 q = [1,2]
 r = p
-print(p==q, p is q, p is r) # True , False , True
+print(p == q, p is q, p is r) # True , False , True
 print(id(p),id(q),id(r)) #совпадет r и p
 
-#Задача номер №4
+#Задача номер 4
 
-nums=[1,2,3,4]
-z = nums
-z.append(5)
-print(z is nums,z)
-print(id(z),id(nums))
-nums_copy=nums.copy()
-print(nums_copy==nums, nums_copy is nums)
+nums = [1,2,3]
+alias = nums
+alias.append(4)
+print("alias is nums", alias is nums,alias)
+print(id(alias),id(nums))
+nums_copy = nums.copy()
+print(nums_copy == nums, nums_copy is nums)
